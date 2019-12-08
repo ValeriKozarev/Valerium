948a015375b8f484626da5bc48890b0a166a48e5
vikozare <vikozare@ncsu.edu>
2019-12-08 14:15:21 -0500

    writing sample flask endpoints

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,9 @@
 import time
 import json
 import hashlib
+from textwrap import dedent
+from uuid import uuid4
+from flask import Flask, jsonify, request
 
 
 class Blockchain(object):
@@ -56,11 +59,33 @@ class Blockchain(object):
 
         return self.get_last_block['index'] + 1
 
-    def create_pow(self):
-        pass
+    def create_pow(self, proof_of_previous):
+        """
+        Simple Proof-of-Work algorithm:
+            1) Given a number p (proof_of_previous) which is the previous proof
+            2) Find a number g (the new proof that we are generating) such that hash(pg) leads with 4 zeroes
+        :param proof_of_previous: <int>
+        :return: <int> proof of work for the new block
+        """
+        proof = 0
+        while self.validate_pow(proof_of_previous, proof) is False:
+            proof = proof + 1
 
-    def validate_pow(self):
-        pass
+        return proof
+
+    @staticmethod
+    def validate_pow(last_proof, proof):
+        """
+        Validate the proof submitted for the block, should be hard to prove and easy to check.
+        Criteria: does hash(last_proof, proof) have 4 leading zeroes?
+        :param last_proof: <int> proof of last block
+        :param proof: <int> proof of work for the new block
+        :return: True if criteria is met, False if not
+        """
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha3_512(guess).hexdigest()
+
+        return guess_hash[:4] == "0000"
 
     @staticmethod
     def hash(block):
@@ -75,3 +100,71 @@ class Blockchain(object):
     @property
     def get_last_block(self):
         return self.chain[-1]
+
+
+"""
+Flask stuff
+"""
+# instantiate the Flask node
+app = Flask(__name__)
+
+# generate unique address for our node
+identifier = str(uuid4()).replace('-', '')
+
+# instantiate the Valerium blockchain
+blockchain = Blockchain()
+
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    # run proof-of-work algorithn
+    last_block = blockchain.get_last_block
+    last_proof = last_block["proof"]
+    proof = blockchain.create_pow(last_proof)
+
+    # receive reward on successful mine (indicate with sender = "0"
+    blockchain.create_transaction("0", identifier, 1)
+
+    # forge the new block
+    previous_hash = blockchain.hash(last_block)
+    new_block = blockchain.create_block(proof, previous_hash)
+
+    resp = {
+        "message": "new block has been forged",
+        "index": new_block["index"],
+        "transactions": new_block["transactions"],
+        "proof": new_block["proof"],
+        "previous_hash": new_block["previous_hash"]
+    }
+
+    return jsonify(resp), 201
+
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    # store the request
+    values = request.get_json()
+
+    # validate the request
+    required_fields = ["sender", "recipient", "amount"]
+    if not all(k in values for k in required_fields):
+        return "Invalid request format", 400
+
+    # create the new transaction
+    index = blockchain.create_transaction(values["sender"], values["recipient"], values["amount"])
+    resp = {"message": f'Transaction will be added to block at index {index}'}
+
+    return jsonify(resp), 201
+
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=9000)
